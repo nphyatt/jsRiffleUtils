7e38b3509aa20e2560204f5d094ac38ad5661855
Nicholas Hyatt <nhyatt@wisc.edu>
2016-04-18 12:36:46 -0500

    use prompt for token getter

--- README.md
+++ README.md
@@ -9,8 +9,6 @@
 > Retrieve a token for a particular domain that you can use to join the fabric with. Tokens are good for 60 days.
 
 ```
-//username and password is your Exis developer login
-//domain is the domain for which the token should be valid
-//name is the name you'd like to give the token for later retrieval/deletion
-node token_getter.js -u username -p password -d domain -n name
+//follow prompt
+node token_getter.js
 ```
--- token_getter.js
+++ token_getter.js
@@ -1,42 +1,78 @@
 var riffle = require('jsriffle');
-var argv = require('minimist')(process.argv.slice(2));
-
-if(Object.keys(argv).length < 5){
-  console.log("Usage: node token_getter.js -p password -u username -d domain -n name\n");
-  process.exit(1);
-}
-
-var xs = riffle.Domain('xs.demo');
-var auth = xs.subdomain('Auth');
-var user = {username: argv.u, password: argv.p};
-var token_name = argv.n;
-var domain = argv.d
-
-xs.login(user).then(join, error.bind({call: "login"}));
-
-function onJoin(){
-  console.log('joined');
-  auth.call('gen_custom_token', token_name, domain).then(genSuccess, error.bind({call: "gen_custom_token"}));
-}
-
-function genSuccess(ok){
-  if(ok){
-    auth.call('get_custom_token', token_name, domain).then(getSuccess, error.bind({call: "get_custom_token"}));
-  }else{
-    console.log("failure to gen token");
+var prompt = require('prompt');
+var schema = {
+  properties: {
+    username: { 
+      type: 'string',
+      pattern: /^[a-zA-Z0-9]+$/,
+      description: "Enter your Exis developer login",
+      message: "usernames are only letters and numbers",
+      required: true
+    },
+    password: {
+     type: 'string',
+     hidden: true,
+     required: true
+    },
+    domain: {
+     type: 'string',
+     pattern: /^[a-z0-9\.]+$/,
+     description: "Enter the domain you would like a token for.",
+     message: "domains are only lowercase letters and numbers seperated by dots i.e. xs.nick2",
+     required: true
+    },
+    name: {
+     type: 'string',
+     description: "Enter a name for the token.",
+     required: true
+    }
   }
 }
 
-function getSuccess(token){
-  console.log(token);
-}
+prompt.start();
 
-function error(error){
-  console.log(this.call, ' : ', error);
-}
+prompt.get(schema, getToken);
 
-function join(user){
-  console.log(user);
-  user.onJoin = onJoin.bind(user);
-  user.join();
+function getToken(err, result){
+
+  if(err){
+    console.log(err);
+    process.exit(1);
+  }
+  var token_name = result.name;
+  var domain = result.domain;
+
+  var xs = riffle.Domain('xs.demo');
+  var auth = xs.subdomain('Auth');
+
+
+
+  xs.login(result).then(join, error.bind({call: "login"}));
+
+  function onJoin(){
+    console.log('joined');
+    auth.call('gen_custom_token', token_name, domain).then(genSuccess, error.bind({call: "gen_custom_token"}));
+  }
+
+  function genSuccess(ok){
+    if(ok){
+      auth.call('get_custom_token', token_name, domain).then(getSuccess, error.bind({call: "get_custom_token"}));
+    }else{
+      console.log("failure to gen token");
+    }
+  }
+
+  function getSuccess(token){
+    console.log(token);
+  }
+
+  function error(error){
+    console.log(this.call, ' : ', error);
+  }
+
+  function join(user){
+    console.log(user);
+    user.onJoin = onJoin.bind(user);
+    user.join();
+  }
 }
